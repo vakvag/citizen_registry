a12fdb459e21323413ee09c95ffef6c977324378
Evangelos Vakondios <48967127+vakvag@users.noreply.github.com>
2026-07-25 19:19:43 +0300

    Add integration test in SampleIT.java

---
+++ citizen-registry/citizen-service/src/test/java/com/citizenregistry/SampleIT.java
@@ -0,0 +1,11 @@
+package com.citizenregistry;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleIT {
+    @Test
+    void myFirstIntegrationTest() {
+        assertTrue(true, "Αυτό είναι ένα Integration Test");
+    }
+}

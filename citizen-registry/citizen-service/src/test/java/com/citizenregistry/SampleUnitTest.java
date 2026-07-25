2a8868b85ac9a7682df87a3444294f47f4d9e413
Evangelos Vakondios <48967127+vakvag@users.noreply.github.com>
2026-07-25 19:19:03 +0300

    Add SampleUnitTest for basic unit testing

---
+++ citizen-registry/citizen-service/src/test/java/com/citizenregistry/SampleUnitTest.java
@@ -0,0 +1,11 @@
+package com.citizenregistry;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleUnitTest {
+    @Test
+    void myFirstUnitTest() {
+        assertTrue(true, "Αυτό είναι ένα Unit Test");
+    }
+}
